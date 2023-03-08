d60181615e765cc640ab8bc2fd4bae3ba01b82eb
Amir <99445613+freddy1911@users.noreply.github.com>
2023-03-08 20:40:51 +0300

    Initial commit

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+class User(AbstractUser):
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
